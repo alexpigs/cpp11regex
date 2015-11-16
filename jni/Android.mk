c1ec502c3c1262531aeff956fba994f177e3f71d
zhongxinger <zhongxinger@kingsoft.com>
2015-11-16 13:45:47 +0800

    asdfasdf

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,5 +7,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := cpp11regex
 LOCAL_SRC_FILES := ./src/cpp11regex.cpp ./src/timer.cpp
-LOCAL_CFLAGS := -std=gnu++11
+LOCAL_CFLAGS := -std=gnu++11 -frtti
 include $(BUILD_EXECUTABLE)
